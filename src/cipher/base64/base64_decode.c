5de72e95e9661f964b9199e80cc32a92e6a28446
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-04-16 15:58:22 +0200

    fix: fix base64 decoding

--- src/cipher/base64/base64_decode.c
+++ src/cipher/base64/base64_decode.c
@@ -13,40 +13,31 @@ static char	base64_get_index(char c)
 static void	base64_decode_nbytes(char (*decoded)[3], const char *buffer, size_t size)
 {
 	char	indexes[4];
-	size_t	j = 0;
 
 	for (size_t i = 0; i < 4; i++)
 	{
-		while (j < size && is_whitespace(buffer[j]))
-			j++;
-		if (j >= size)
+		if (i >= size)
 			indexes[i] = 0;
-		else if (buffer[j] == '=')
+		else if (buffer[i] == '=')
 			indexes[i] = 0;
 		else
-			indexes[i] = base64_get_index(buffer[j]);
-		j++;
+			indexes[i] = base64_get_index(buffer[i]);
 	}
 	(*decoded)[0] = ((indexes[0] & 0x3F) << 2) | ((indexes[1] & 0x30) >> 4);
 	(*decoded)[1] = ((indexes[1] & 0x0F) << 4) | ((indexes[2] & 0x3C) >> 2);
 	(*decoded)[2] = ((indexes[2] & 0x03) << 6) | (indexes[3] & 0x3F);
 }
 
-char	*base64_decode(const char *buffer, size_t size, size_t *output_size)
+static char	*base64_decode_raw(const char *buffer, size_t size, size_t *output_size)
 {	
 	char	*decoded;
 	char	temp[3];
 	size_t	remaining;
 	size_t	decoded_size;
-	size_t	valid_chars = 0;
+	size_t	cpy_size;
 
-	for (size_t i = 0; i < size; i++)
-	{
-		if (!is_whitespace(buffer[i]))
-			valid_chars++;
-	}
-	decoded_size = valid_chars * 3 / 4 + 1;
-	decoded = calloc(decoded_size, sizeof(char));
+	decoded_size = size * 3 / 4 + 3;
+	decoded = calloc(decoded_size + 1, sizeof(char));
 	if (!decoded)
 		return (NULL);
 	decoded_size = 0;
@@ -54,37 +45,59 @@ char	*base64_decode(const char *buffer, size_t size, size_t *output_size)
 	{
 		remaining = size - i;
 		base64_decode_nbytes(&temp, buffer + i, remaining);
-		memcpy(decoded + decoded_size, temp, 3);
-		decoded_size += 3;
+		cpy_size = remaining >= 4 ? 3 : remaining * 3 / 4;
+		memcpy(decoded + decoded_size, temp, cpy_size);
+		decoded_size += cpy_size;
 	}
 	*output_size = decoded_size;
 	return (decoded);
 }
 
+char	*base64_decode(const char *buffer, size_t size, size_t *output_size)
+{	
+	char	*decoded;
+	char	*new_input;
+	size_t	new_size = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!is_whitespace(buffer[i]) && buffer[i] != '=')
+			new_size++;
+	}
+	new_input = calloc(new_size + 1, sizeof(char));
+	if (!new_input)
+		return (NULL);
+	new_size = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!is_whitespace(buffer[i]) && buffer[i] != '=')
+			new_input[new_size++] = buffer[i];
+	}
+	decoded = base64_decode_raw(new_input, new_size, output_size);
+	free(new_input);
+	return (decoded);
+}
+
 int	base64_check_encoded(const char *buffer, size_t size)
 {
-	int		has_padding = 0;
+	int		padding = 0;
 	size_t	valid_chars = 0;
 
 	if (!buffer || size == 0)
-		return (1);
+		return (0);
 	for (size_t i = 0; i < size; i++)
 	{
 		if (is_whitespace(buffer[i]))
 			continue;
 		valid_chars++;
 		if (buffer[i] == '=')
-		{
-			has_padding = 1;
-			if (i < size - 2)
-				return (1);
-		}
-		else if (has_padding)
+			padding++;
+		else if (padding)
 			return (1);
 		else if (!(base64_get_index(buffer[i]) < sizeof(BASE64_ALPHABET)))
 			return (1);
 	}
-	if (valid_chars % 4 != 0)
+	if (valid_chars % 4 != 0 || padding > 2)
 		return (1);
 	return (0);
 }
